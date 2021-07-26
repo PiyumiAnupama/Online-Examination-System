a1765d60c56c1ff1bef929553c3cb05a6ffe553e
PiyumiAnupama <96477@fhss.sjp.ac.lk>
2021-07-27 02:01:15 +0530

    connect student GUI

--- Online_Examination_System/src/online_examination_system/QuizSubjects.java
+++ Online_Examination_System/src/online_examination_system/QuizSubjects.java
@@ -178,42 +178,42 @@ public class QuizSubjects extends javax.swing.JFrame {
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
-        CreateQuiz q=new CreateQuiz();
+        CreateQuizScience q=new CreateQuizScience();
                     q.setVisible(true);
                     this.setVisible(false);
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        CreateQuiz q=new CreateQuiz();
+        CreateQuizBuddhism q=new CreateQuizBuddhism();
                     q.setVisible(true);
                     this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        CreateQuiz q=new CreateQuiz();
+        CreateQuizSinhala q=new CreateQuizSinhala();
                     q.setVisible(true);
                     this.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-        CreateQuiz q=new CreateQuiz();
+        CreateQuizEnglish q=new CreateQuizEnglish();
                     q.setVisible(true);
                     this.setVisible(false);
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
-        CreateQuiz q=new CreateQuiz();
+        CreateQuizHistory q=new CreateQuizHistory();
                     q.setVisible(true);
                     this.setVisible(false);
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        CreateQuiz q=new CreateQuiz();
+        CreateQuizMathematics q=new CreateQuizMathematics();
                     q.setVisible(true);
                     this.setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
